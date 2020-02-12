b491ac2005adc150d7dc418a4d2ae51ec6f07dd2
larjia <yongzhen.jiang@outlook.com>
2020-02-12 08:58:18 +0800

    NavBar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,23 @@
 <template>
-  <h2>Home</h2>
+  <nav-bar class="home-nav">
+    <div slot="center">购物街</div>
+  </nav-bar>
 </template>
 
 <script>
+import NavBar from '@/components/common/navbar/NavBar'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
