58739c3fe28e4792be7a40dea1032ad4b2857663
larjia <yongzhen.jiang@outlook.com>
2020-02-13 11:32:39 +0800

    add feature view

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,21 +1,73 @@
 <template>
-  <nav-bar class="home-nav">
-    <div slot="center">购物街</div>
-  </nav-bar>
+  <div id="home" class="wrapper">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
+    <feature-view/>
+
+    <ul>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+      <li>hello</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import NavBar from '@/components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+
+import { getHomeMultidata } from '@/network/home'
 
 export default {
   name: 'Home',
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView
+  },
+  data () {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created () {
+    getHomeMultidata().then(res => {
+      // console.log(res)
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+      console.log(this.banners)
+    })
   }
 }
 </script>
 
 <style scoped>
+  #home {
+    height: 100vh;
+    position: relative;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: white;
